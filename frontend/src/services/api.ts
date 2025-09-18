391c9b9ed2b48276a13fc080dc28616316949bcd
Mehmet Kaplan <135678975+MehmetKaplan33@users.noreply.github.com>
2025-09-18 20:17:35 +0300

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://task-manager-production-075d.up.railway.app/';
+const API_BASE_URL = 'https://task-manager-production-075d.up.railway.app/api';
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
